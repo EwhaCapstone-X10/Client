0ddcef4e2218181067a18b7049823754cbcaf73f
AnSui22 <ansui218@ewhain.net>
2025-01-19 18:00:47 +0900

    feat: 기본 정보 입력 기능 구현

--- src/screens/AgeForm.tsx
+++ src/screens/AgeForm.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import FormInput from '../components/FormInput';
+import { Text, View, TextInput } from 'react-native';
 import MainButton from '../components/MainButton';
+import { useState } from 'react';
 
 const AgeForm = () => {
+  const [age, onChangeAge] = useState('');
+  const handleNext = () => {
+    // 버튼 클릭 시 백에 정보 보내거나 localstorage에 저장
+  };
   return (
     <View className="flex-1">
       <View className="left-view gap-3">
         <Text className="custom-title">나이를 입력해주세요</Text>
         <Text className="custom-description">숫자만 입력해주세요</Text>
       </View>
-      <View className="flex-1 w-full px-7 py-6 gap-4">
-        <FormInput title="나이" keyboardType="number-pad" />
+      <View className="flex-1 left-view gap-1">
+        <Text className="custom-inputinfo">나이</Text>
+        <TextInput className="custom-input" onChangeText={onChangeAge} value={age} keyboardType="number-pad" />
       </View>
-      <MainButton text="다음" nav="GenderForm" />
+      <MainButton text="다음" nav="GenderForm" onClick={handleNext} isAbled={!age} />
     </View>
   );
 };
--- src/screens/JobForm.tsx
+++ src/screens/JobForm.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import FormInput from '../components/FormInput';
+import React, { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
 import MainButton from '../components/MainButton';
 
 const JobForm = () => {
+  const [job, onChangeJob] = useState('');
+  const handleSubmit = () => {
+    // 버튼 클릭 시 백에 정보 보내거나 localstorage에 저장
+  };
   return (
     <View className="flex-1">
       <View className="left-view gap-3">
         <Text className="custom-title">직업을 입력해주세요</Text>
         <Text className="custom-description">필수 입력은 아닙니다 !</Text>
       </View>
-      <View className="flex-1 w-full px-7 py-6 gap-4">
-        <FormInput title="직업" keyboardType="default" />
+      <View className="flex-1 left-view gap-1">
+        <Text className="custom-inputinfo">직업</Text>
+        <TextInput className="custom-input" onChangeText={onChangeJob} value={job} />
       </View>
-      <MainButton text="완료" nav="Main" />
+      <MainButton text="완료" nav="Main" onClick={handleSubmit} isAbled={false} />
     </View>
   );
 };
