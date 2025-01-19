cc168329daa98050e5bb9d25ab5c278f0a837da2
AnSui22 <ansui218@ewhain.net>
2025-01-19 16:35:56 +0900

    feat: Splash 화면 구현

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,20 +1,21 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
 import { RootStackParamList } from '../models/navigation.model';
 
 const Splash = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate('Entry');
+    }, 2500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-primary_500 text-5xl font-Bold text-center">Drive Mate</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('StretchingList')}>
-        <Text className="text-2xl">StretchingList</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Entry')}>
-        <Text className="text-2xl">Entry</Text>
-      </TouchableOpacity>
     </View>
   );
 };
